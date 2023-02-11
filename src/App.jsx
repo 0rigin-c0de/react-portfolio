075441daec111d156c6aa1a0cb61dcc215c52639
0rigin-c0de <shunnu807@gmail.com>
2023-02-11 10:19:13 +0530

    initial-commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Nav from './components/Nav'
+import Home from './components/Home'
+import About from './components/About'
+import Portfolio from './components/Portfolio'
+import Experience from './components/Experience'
+import Contact from './components/Contact'
+import SocialLinks from './components/SocialLinks'
+import "./index.css";
+
+function App() {
+
+  return (
+    <div>
+      <Nav />
+      <Home />
+      <About />
+      <Portfolio />
+      <Experience />
+      <Contact />
+      <SocialLinks />
+    </div>
+  )
+}
+
+export default App
